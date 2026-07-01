46ffa22e268d67dbf6aa3c3ed05b155efb83fe2f
MoayadBadarnh <your-email@gmail.com>
2026-07-01 08:44:15 +0300

    Fix typescript default keyword error in models schema

--- models.ts
+++ models.ts
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
-githubPagesUrl: { type: String, default: "" },
-isPublishedToGithub: { type: Boolean, default: false },
-
+githubPagesUrl: { type: String, 'default': "" },
+isPublishedToGithub: { type: Boolean, 'default': false },
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true, lowercase: true, trim: true },
   createdAt: { type: Date, default: Date.now }
